05afc45e6a2ea2d893203126bdadddb19c55cf23
Nadikatla <S.Nadikatla@shell.com>
2021-06-21 21:34:18 +0530

    tt

---
+++ src/taskpane/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+// images references in the manifest
+import "../../../assets/icon-16.png";
+import "../../../assets/icon-32.png";
+import "../../../assets/icon-80.png";
+const template = require("./app.component.html");
+/* global console, Excel, require */
+
+@Component({
+  selector: "app-home",
+  template,
+})
+export default class AppComponent {
+  welcomeMessage = "Welcome";
+
+  async run() {
+    try {
+      await Excel.run(async (context) => {
+        /**
+         * Insert your Excel code here
+         */
+        const range = context.workbook.getSelectedRange();
+
+        // Read the range address
+        range.load("address");
+
+        // Update the fill color
+        range.format.fill.color = "yellow";
+
+        await context.sync();
+        console.log(`The range address was ${range.address}.`);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
